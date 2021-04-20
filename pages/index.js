965e96a5712de61238906d7c6b54e992fdc0bb3a
Emre Kelleci <emrek@Admins-MacBook-Pro.local>
2021-04-20 09:46:37 -0400

    trial branch created

--- pages/index.js
+++ pages/index.js
@@ -8,8 +8,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1> Hello this is Emre </h1>
+      <h1> Hello this is Emre! </h1>
 
+      <p> This is the trial page addition</p>
       
     </div>
   )
